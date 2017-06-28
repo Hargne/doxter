19fbe263344558e784f5b0e52b09127d64b05177
Johan Hargne <Johan.Hargne@kindredplc.com>
2017-06-28 15:32:14 +0200

    Added types, Restructured folders, Added eslint, Fixed tabs

--- test/test2.js
+++ test/test2.js
@@ -1,7 +1,18 @@
 /**
- * @doxter-define Test2
+ * @doxterMethod
+ * @param  {String} input A string
+ * @return {Bool}
  */
-function test2() {
-	/** @doxter-return {String} A greeting */
+function test2(input) {
+	return true;
+}
+
+/**
+ * @doxterMethod
+ * @param  {Number} input A Number
+ * @param  {Object} input2 An Object
+ * @return {String}
+ */
+function test3({ input, input2 }) {
 	return 'hej2';
 }
